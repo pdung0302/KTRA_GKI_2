9fb5a422e849d9f01581256e825b6eb03b3e5756
pdung0302 <21010613@st.phenikaa-uni.edu.vn>
2024-05-24 15:00:32 +0700

    update

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,7 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'HelloWorld';
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,7 +34,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
+    <!-- <nav class="top-nav">
         <div class="top-nav-title">
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
@@ -42,7 +42,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
         </div>
-    </nav>
+    </nav> -->
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
