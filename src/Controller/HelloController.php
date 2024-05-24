2e078565a709c117b667ea1428c9bbba0e387948
pdung0302 <21010613@st.phenikaa-uni.edu.vn>
2024-05-24 14:39:08 +0700

    upload

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class HelloController extends AppController
+{
+    public function index()
+    {
+        $this->set('message', 'Hello World');
+    }
+}
+
+?>
